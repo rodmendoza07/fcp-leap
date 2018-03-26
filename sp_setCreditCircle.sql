4f24f617883b6f5a1f8fd7633fc7543f238ebc1c
rodrigoMendoza <luis.mendoza@grupoporvenir.com.mx>
2018-03-26 10:40:34 -0600

    scripts faltantes

---
+++ sp_setCreditCircle.sql
@@ -0,0 +1,69 @@
+USE LEAP
+GO
+
+ALTER PROCEDURE [dbo].[sp_setCreditCircle] (
+	@udt_circulocredito udt_circulocredito READONLY
+	, @udt_circulocredito_home udt_circulocredito_domicilio READONLY
+)
+AS
+BEGIN
+	DECLARE
+		@msg VARCHAR(300)
+		, @lastId INT
+
+	BEGIN TRY
+		BEGIN TRAN
+		INSERT INTO LEAP.dbo.tp_circuloCredito (
+			cc_nombre
+			, cc_paterno
+			, cc_materno
+			, cc_birthdate
+			, cc_rfc
+			, cc_curp
+			, cc_folioquery
+			, cc_folioquerySic
+			, cc_querydate
+			, cc_totalAprobed
+			, cc_montoAprobado
+			, cc_saldoActual
+			, cc_saldoVenc
+			, cc_paymentMonth
+		)
+		SELECT
+			*
+		FROM @udt_circulocredito
+
+		SET @lastId = @@IDENTITY
+
+		INSERT INTO LEAP.dbo.td_circulocredito_domicilios (
+			ccd_cc_id
+			, ccd_calleNumero
+			, ccd_numeroint
+			, ccd_numeroext
+			, ccd_colonia
+			, ccd_municipio
+			, ccd_estado
+		)
+		SELECT
+			@lastId
+			, ccd_calleNumero
+			, ccd_numeroint
+			, ccd_numeroext
+			, ccd_colonia
+			, ccd_municipio
+			, ccd_estado
+		FROM @udt_circulocredito_home
+
+		IF @@TRANCOUNT > 0 
+			COMMIT TRAN
+		ELSE
+			ROLLBACK TRAN
+			SET @msg = (SELECT SUBSTRING(ERROR_MESSAGE(), 1, 300))
+	END TRY
+	BEGIN CATCH
+		ROLLBACK TRAN
+		SET @msg = (SELECT SUBSTRING(ERROR_MESSAGE(), 1, 300))
+		RAISERROR(@msg, 16, 1)
+	END CATCH
+	
+END
---
+++ udt_circulocredito.sql
@@ -0,0 +1,21 @@
+USE [LEAP]
+GO
+
+CREATE TYPE [dbo].[udt_circulocredito] AS TABLE(
+	cc_idPros INT NOT NULL
+	, cc_nombre VARCHAR(50) NOT NULL
+	, cc_paterno VARCHAR(50) NOT NULL
+	, cc_materno VARCHAR(50) NOT NULL
+	, cc_birthdate DATETIME NOT NULL
+	, cc_rfc VARCHAR(20) NOT NULL
+	, cc_curp VARCHAR(25) NOT NULL
+	, cc_folioquery INT NOT NULL
+	, cc_folioquerySic INT NOT NULL
+	, cc_querydate DATETIME NOT NULL
+	, cc_totalAprobed INT NOT NULL
+	, cc_montoAprobado DECIMAL(18,4) NOT NULL
+	, cc_saldoActual DECIMAL(18,4) NOT NULL
+	, cc_saldoVenc DECIMAL(18,4) NOT NULL
+	, cc_paymentMonth DECIMAL(18,4) NOT NULL
+)
+GO
